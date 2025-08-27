984029ee0dc224bc9658625112c63045196a9fc1
flyeers <juliafluper@gmail.com>
2025-08-28 00:19:20 +0200

    Rotation set axis

--- Assets/Scripts/Interactions/RotateInteract.cs
+++ Assets/Scripts/Interactions/RotateInteract.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class RotateInteract : MonoBehaviour, IInteractableHeld
 {
+    public enum RotationAxis { X, Y, Z }
+    [SerializeField] private RotationAxis axis = RotationAxis.Y;
+    [SerializeField] private float step = 45f;
+
     public void InteractHeldRight(GameObject interactor)
     {
-        Vector3 localEuler = gameObject.transform.localEulerAngles;
-        localEuler.y -= 45f;
-        gameObject.transform.localRotation = Quaternion.Euler(localEuler);
+        Rotate(-step);
     }
+
     public void InteractHeldLeft(GameObject interactor)
     {
-        Vector3 localEuler = gameObject.transform.localEulerAngles;
-        localEuler.y += 45f;
-        gameObject.transform.localRotation = Quaternion.Euler(localEuler);
+        Rotate(step);
+    }
+
+    private void Rotate(float angle)
+    {
+        Vector3 rotationVector = Vector3.zero;
+
+        switch (axis)
+        {
+            case RotationAxis.X: 
+                rotationVector = Vector3.right * angle; 
+                break;
+            case RotationAxis.Y: 
+                rotationVector = Vector3.up * angle; 
+                break;
+            case RotationAxis.Z: 
+                rotationVector = Vector3.forward * angle; 
+                break;
+        }
+
+        transform.Rotate(rotationVector, Space.Self);
     }
 }
