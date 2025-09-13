91ba185b5b52a0e96e14ed6e591a098ebcf01a5a
flyeers <juliafluper@gmail.com>
2025-09-13 23:40:31 +0200

    Device identification in messages - prep for image instead of text

--- Assets/Scripts/Interactions/MessageInteract.cs
+++ Assets/Scripts/Interactions/MessageInteract.cs
@@ -7,11 +7,25 @@ using UnityEngine.UI;
 public class MessageInteract : MonoBehaviour, IInteractable
 {
     public GameObject messageUIPrefab;
+
+    public enum TypeOption
+    {
+        Text,
+        Image
+    }
+    [SerializeField] private TypeOption type;
+
+    [Header("If Text")]
     [TextArea(3, 10)] 
     public string messageText = "Mago fumon mueve habitacion";
 
-    [SerializeField] private Sprite _keboardImg;
-    [SerializeField] private Sprite _gamepadImg;
+    [Header("If Image")]
+    [SerializeField] private Sprite _keboardInfoImg;
+    [SerializeField] private Sprite _gamepadInfoImg;
+
+    [Header("Exit info")]
+    [SerializeField] private Sprite _keboardExitImg;
+    [SerializeField] private Sprite _gamepadExitImg;
 
     public void Interact(GameObject interactor)
     {
@@ -28,20 +42,38 @@ public class MessageInteract : MonoBehaviour, IInteractable
                 {
                     if (deviceDetector.IsUsingKeyboard()) 
                     {
-                        exitImage.gameObject.GetComponent<Image>().sprite = _keboardImg;
+                        if(_keboardExitImg) exitImage.gameObject.GetComponent<Image>().sprite = _keboardExitImg;
                     }
                     else 
                     {
-                        exitImage.gameObject.GetComponent<Image>().sprite = _gamepadImg;
+                        if (_gamepadExitImg) exitImage.gameObject.GetComponent<Image>().sprite = _gamepadExitImg;
                     }
                 }              
             }
 
-            //Text
-            TextMeshProUGUI tmpText = currentMessageUI.GetComponentInChildren<TextMeshProUGUI>();
-            if (tmpText != null)
-                tmpText.text = messageText;        
-            
+            if(type == TypeOption.Text) //Text
+            {
+                TextMeshProUGUI tmpText = currentMessageUI.GetComponentInChildren<TextMeshProUGUI>();
+                if (tmpText != null)
+                    tmpText.text = messageText;    
+            }
+            else  //Image
+            {
+                //Info image based on current device
+                Transform infoImage = currentMessageUI.transform.Find("InfoImage");
+                if (infoImage != null && infoImage.gameObject)
+                {
+                    if (deviceDetector.IsUsingKeyboard())
+                    {
+                        if (_keboardInfoImg) infoImage.gameObject.GetComponent<Image>().sprite = _keboardInfoImg;
+                    }
+                    else
+                    {
+                        if (_gamepadInfoImg) infoImage.gameObject.GetComponent<Image>().sprite = _gamepadInfoImg;
+                    }
+                }
+            }
+
             Time.timeScale = 0f;
         }
     }
